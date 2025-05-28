3444c03627cd7ac0c2db91d03b4ea64059278452
力久凌太郎 <oniku@ryokukyuuryoutarounoMacBook-Pro.local>
2025-05-29 01:45:37 +0900

    はじめてのコミット

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-10 bg-white text-black">
+      <h1 className="text-5xl font-bold mb-4">※＜こんにちは！アケルのアナルです！</h1>
+      <p className="text-lg">なめていただけますか？</p>
+    </main>
+  );
+}
